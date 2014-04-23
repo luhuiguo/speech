ce49607498450386d58431570b8c026532b385f0
luhuiguo <luhuiguo@gmail.com>
2014-04-23 22:10:53 +0800

    .

---
+++ src/main/java/com/luhuiguo/speech/AmrLibrary.java
@@ -0,0 +1,19 @@
+package com.luhuiguo.speech;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.Pointer;
+
+public interface AmrLibrary extends Library {
+
+	AmrLibrary INSTANCE = (AmrLibrary) Native.loadLibrary(
+			"opencore-amrnb", AmrLibrary.class);
+	
+	Pointer Decoder_Interface_init();
+	void Decoder_Interface_exit(Pointer state);
+	void Decoder_Interface_Decode(Pointer state, byte[] in, short[] out, int bfi);
+	Pointer Encoder_Interface_init(int dtx);
+	void Encoder_Interface_exit(Pointer state);
+	int Encoder_Interface_Encode(Pointer state,  int mode, short[] speech, byte[] out, int forceSpeech);
+
+}
---
+++ src/main/java/com/luhuiguo/speech/Mode.java
@@ -0,0 +1,14 @@
+package com.luhuiguo.speech;
+
+public enum Mode {
+	MR475, /* 4.75 kbps */
+	MR515, /* 5.15 kbps */
+	MR59, /* 5.90 kbps */
+	MR67, /* 6.70 kbps */
+	MR74, /* 7.40 kbps */
+	MR795, /* 7.95 kbps */
+	MR102, /* 10.2 kbps */
+	MR122, /* 12.2 kbps */
+	MRDTX, /* DTX */
+	N_MODES;
+}
